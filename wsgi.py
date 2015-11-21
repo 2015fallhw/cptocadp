6d702bbf28fa06ed37c09039deeab13712c48651
coursemdetw <course@mde.tw>
2015-11-21 23:30:46 +0800

    initial add

---
+++ wsgi.py
@@ -0,0 +1,93 @@
+# 以下為宣告 ################################## (1)
+#!/usr/bin/python
+# 導入 os 模組, 主要用來判斷是否在 OpenShift 上執行
+import os
+from flask import Flask, request, redirect, \
+                render_template, session
+import random
+
+# 以下為全域變數宣告 ############################# (2)
+# 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
+_curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
+
+# 設定在雲端與近端的資料儲存目錄
+if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+    # 表示程式在雲端執行
+    download_root_dir = os.environ['OPENSHIFT_DATA_DIR']
+    data_dir = os.environ['OPENSHIFT_DATA_DIR']
+    template_root_dir = os.environ['OPENSHIFT_REPO_DIR']+"/static"
+else:
+    # 表示程式在近端執行
+    download_root_dir = _curdir + "/local_data/"
+    data_dir = _curdir + "/local_data/"
+    template_root_dir = _curdir + "/static"
+
+# 啟動 app
+app = Flask(__name__)
+
+# 使用 session 必須要設定 secret_key
+app.secret_key = 'A0Zr9@8j/3yX R~XHH!jmN]LWX/,?R@T'
+
+# 以下為相關各方法的定義 ########################### (3)
+@app.route("/")
+def index():
+    #這是猜數字遊戲的起始表單, 主要在產生答案, 並且將 count 歸零
+    # 將標準答案存入 answer session 對應區
+    theanswer = random.randint(1, 100)
+    thecount = 0
+    # 將答案與計算次數變數存進 session 對應變數
+    session['answer'] = theanswer
+    session['count'] = thecount
+
+    return render_template("index.html", answer=theanswer, count=thecount)
+@app.route('/guessform')
+def guessform():
+    session["count"] += 1
+    guess = session.get("guess")
+    theanswer = session.get("answer")
+    count = session.get("count")
+    
+    return render_template("guessform.html", guess=guess, answer=theanswer, count=count)
+@app.route('/docheck', methods=['POST'])
+def docheck():
+    # session[] 存資料
+    # session.get() 取 session 資料
+    # 利用 request.form[] 取得表單欄位資料, 然後送到 template
+    guess = request.form["guess"]
+    session["guess"] = guess
+    # 假如使用者直接執行 doCheck, 則設法轉回根方法
+    if guess is None:
+        redirect("/")
+    # 從 session 取出 answer 對應資料, 且處理直接執行 docheck 時無法取 session 值情況
+    try:
+        theanswer = int(session.get('answer'))
+    except:
+        redirect("/")
+    # 經由表單所取得的 guess 資料型別為 string
+    try:
+        theguess = int(guess)
+    except:
+        return redirect("/guessform")
+    # 每執行 doCheck 一次,次數增量一次
+    session["count"] += 1
+    count = session.get("count")
+    # 答案與所猜數字進行比對
+    if theanswer < theguess:
+        return render_template("toobig.html", guess=guess, answer=theanswer, count=count)
+    elif theanswer > theguess:
+        return render_template("toosmall.html", guess=guess, answer=theanswer, count=count)
+    else:
+        # 已經猜對, 從 session 取出累計猜測次數
+        thecount = session.get('count')
+        return "猜了 "+str(thecount)+" 次, 終於猜對了, 正確答案為 "+str(theanswer)+": <a href='/'>再猜</a>"
+    # 應該不會執行下列一行
+    return render_template("docheck.html", guess=guess)
+
+# 以下開始判斷在 OpenShift 或近端執行 ################### (4)
+if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+    # 表示程式在雲端執行
+    application = app
+else:
+    # 表示在近端執行
+    app.run(debug=True)
+
